78e8db4e9ca983861483f5a145d38431dac481cd
JoeTodes <jtwilson.home@gmail.com>
2021-09-22 22:47:32 -0600

    added tests for GET, POST, and DELETE

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const { query } = require("express");
 const express = require("express");
 const Datastore = require("nedb");
 
@@ -8,10 +9,61 @@ app.use(express.json({ limit: "1mb" }));
 const database = new Datastore("data.db");
 database.loadDatabase();
 
+const sampleData = {
+	sometext: "hihihihi",
+	aNumber: 5,
+	anArray: ["hi", 7],
+	anObject: { childOne: "Autumn", childTwo: "Rae", nChildren: 2 },
+	_id: "4RlVy5qmAbNyxy5m",
+};
+
 app.get("/api", (req, res) => {
-  database.find({}, (err, data) => {
-    res.json(data);
-  });
+	database.find({}, (err, data) => {
+		res.json(data);
+	});
+});
+
+app.get("/api/search", (req, res) => {
+	let q = req.query;
+	database.find(q, (err, data) => {
+		if (Object.keys(data).length > 0) {
+			res.json(data);
+		} else {
+			res.status(400);
+			res.json({ error: "no records match search query" });
+		}
+	});
+});
+
+app.post("/api", (req, res) => {
+	let newData = req.body;
+	let valid = true;
+	if (Object.keys(newData).length == 0) {
+		valid = false;
+	} else {
+		for (const key in newData) {
+			let keyValid = false;
+			for (const prop in sampleData) {
+				if (key == prop) {
+					keyValid = true;
+					break;
+				}
+			}
+			if (!keyValid) {
+				valid = false;
+				break;
+			}
+		}
+	}
+	if (valid) {
+		database.insert(newData, (err, newDoc) => {
+			res.status(201);
+			res.json(newDoc);
+		});
+	} else {
+		res.status(400);
+		res.json({ error: "invalid data" });
+	}
 });
 
 module.exports = app;
--- server.test.js
+++ server.test.js
@@ -1,101 +1,199 @@
 const request = require("supertest");
+const { response } = require("./app");
 const app = require("./app");
 
 describe("serving static files", () => {
-  test("should respond with 200 status code", async () => {
-    const response = await request(app).get("/");
-    expect(response.statusCode).toBe(200);
-  });
+	let response;
+	beforeAll(async () => {
+		response = await request(app).get("/");
+	});
+	test("should respond with 200 status code", () => {
+		expect(response.statusCode).toBe(200);
+	});
 
-  test("should with an html document", async () => {
-    const response = await request(app).get("/");
-    expect(response.headers["content-type"]).toEqual(
-      expect.stringContaining("html")
-    );
-  });
+	test("should with an html document", () => {
+		expect(response.headers["content-type"]).toEqual(
+			expect.stringContaining("html")
+		);
+	});
 });
 
 describe("REST API Behaviour", () => {
-  let sampleData;
-  let badID = "ihopethisisntanID";
-  beforeAll(async () => {
-    let res = await request(app).get("/api");
-    sampleData = res.body[0];
-    delete sampleData["_id"];
-  });
+	let sampleData;
+	let knownID;
+	let badID = "ihopethisisntanID";
+	let newID;
+	beforeAll(async () => {
+		let res = await request(app).get("/api");
+		sampleData = res.body[0];
+		knownID = sampleData._id;
+		delete sampleData["_id"];
+	});
 
-  describe("GET /api", () => {
-    test("should respond with 200 status code", async () => {
-      const response = await request(app).get("/api");
-      expect(response.statusCode).toBe(200);
-    });
-    test("should respond with a JSON object", async () => {
-      const response = await request(app).get("/");
-      expect(response.headers["content-type"]).toEqual(
-        expect.stringContaining("html")
-      );
-    });
-  });
+	describe("GET /api", () => {
+		let response;
+		beforeAll(async () => {
+			response = await request(app).get("/api");
+		});
+		test("should respond with 200 status code", () => {
+			expect(response.statusCode).toBe(200);
+		});
+		test("should respond with a JSON object", () => {
+			expect(response.headers["content-type"]).toEqual(
+				expect.stringContaining("json")
+			);
+		});
+	});
 
-  describe("GET /api/search?{KEY}={VALUE}", () => {
-    describe("when search result exists in database", () => {
-      //should respond with 200 status code
-      //should respond with JSON object of results
-      //results should contain the key and value from the query
-    });
-    describe("when search result does not exist in database", () => {
-      //should respond with 400 status code
-      //should respond with JSON object containing an error message
-    });
-  });
+	describe("GET /api/search?{KEY}={VALUE}", () => {
+		describe("when search result exists in database", () => {
+			let response;
+			beforeAll(async () => {
+				response = await request(app).get(`/api/search?_id=${knownID}`);
+			});
+			test("should respond with 200 status code", () => {
+				expect(response.statusCode).toBe(200);
+			});
+			test("should respond with a JSON object", () => {
+				expect(response.headers["content-type"]).toEqual(
+					expect.stringContaining("json")
+				);
+			});
+			test("results should contain the key and value from the query", () => {
+				expect(response.body[0]._id).toBe(knownID);
+			});
+		});
+		describe("when search result does not exist in database", () => {
+			let response;
+			beforeAll(async () => {
+				response = await request(app).get(`/api/search?_id=${badID}`);
+			});
+			test("should respond with 400 status code", () => {
+				expect(response.statusCode).toBe(400);
+			});
+			test("should respond with a JSON object", () => {
+				expect(response.headers["content-type"]).toEqual(
+					expect.stringContaining("json")
+				);
+			});
+			test("response object should contain an error message", () => {
+				expect(Object.keys(response.body)[0]).toEqual("error");
+			});
+		});
+	});
 
-  describe("POST /api", () => {
-    describe("when request body is valid data for POST", () => {
-      //should respond with 201 status code
-      //should respond with JSON object containing new record in database
-      //response from GET "/api" should then contain the new record
-    });
-    describe("when request body is invalid data for POST", () => {
-      //should respond with 400 status code
-      //should respond with JSON object containing error message
-      //response from GET "/api" should not contain the new record
-    });
-  });
+	describe("POST /api", () => {
+		describe("when request body is valid data for POST", () => {
+			let response;
 
-  describe("PUT /api/:id", () => {
-    describe("when the id exists in the database", () => {
-      describe("when request body is valid data for PUT", () => {
-        //should respond with 200 status code
-        //should respond with JSON object containing updated record in database
-        //response from GET "/api" should then contain the updated record
-      });
-      describe("when request body is invalid data for PUT", () => {
-        //should respond with 400 status code
-        //should respond with JSON object containing error message
-        //response from GET "/api" should not contain the update
-      });
-    });
-    describe("when the id does not exist in the database", () => {
-      describe("when request body is valid data for PUT", () => {
-        //should respond with 201 status code
-        //should respond with JSON object containing new record in database
-        //response from GET "/api" should then contain the new record
-      });
-      describe("when request body is invalid data for PUT", () => {
-        //should respond with 400 status code
-        //should respond with JSON object containing error message
-        //response from GET "/api" should not contain the new record
-      });
-    });
-  });
+			beforeAll(async () => {
+				response = await request(app).post("/api").send(sampleData);
+				newID = response.body._id;
+			});
+			test("should respond with 201 status code", () => {
+				expect(response.statusCode).toBe(201);
+			});
+			test("should respond with a JSON object", () => {
+				expect(response.headers["content-type"]).toEqual(
+					expect.stringContaining("json")
+				);
+			});
+			test("response should match new record in database", async () => {
+				let newRecord = await request(app).get(`/api/search?_id=${newID}`);
+				expect(response.body).toEqual(newRecord.body[0]);
+			});
+		});
+		describe("when request body is invalid data for POST", () => {
+			let response;
+			beforeAll(async () => {
+				response = await request(app)
+					.post("/api")
+					.send({ dontusethiskey: "itisjustfakedata" });
+			});
+			test("should respond with 400 status code", () => {
+				expect(response.statusCode).toBe(400);
+			});
+			test("should respond with a JSON object", () => {
+				expect(response.headers["content-type"]).toEqual(
+					expect.stringContaining("json")
+				);
+			});
+			test("response object should contain an error message", () => {
+				expect(Object.keys(response.body)[0]).toEqual("error");
+			});
+			test("should not add a new record with the invalid data", async () => {
+				let newRecord = await request(app).get(
+					`/api/search?dontusethiskey=itisjustfakedata`
+				);
+				expect(newRecord.statusCode).toBe(400);
+				expect(Object.keys(newRecord.body)[0]).toBe("error");
+			});
+		});
+	});
 
-  describe("DELETE /api/:id", () => {
-    describe("when the id exists in the database", () => {
-      //should respond with 204 status code
-      //response from GET "/api" should not contain record with the id
-    });
-    describe("when the id does not exist in the database", () => {
-      //should respond with 404 status code
-    });
-  });
+	describe("PUT /api/:id", () => {
+		describe("when the id exists in the database", () => {
+			describe("when request body is valid data for PUT", () => {
+				//should respond with 200 status code
+				//should respond with JSON object containing updated record in database
+				//response from GET "/api" should then contain the updated record
+			});
+			describe("when request body is invalid data for PUT", () => {
+				//should respond with 400 status code
+				//should respond with JSON object containing error message
+				//response from GET "/api" should not contain the update
+			});
+		});
+		describe("when the id does not exist in the database", () => {
+			describe("when request body is valid data for PUT", () => {
+				//should respond with 201 status code
+				//should respond with JSON object containing new record in database
+				//response from GET "/api" should then contain the new record
+			});
+			describe("when request body is invalid data for PUT", () => {
+				//should respond with 400 status code
+				//should respond with JSON object containing error message
+				//response from GET "/api" should not contain the new record
+			});
+		});
+	});
+
+	describe("DELETE /api/:id", () => {
+		describe("when the id exists in the database", () => {
+			let response;
+			beforeAll(async () => {
+				if (!newID) {
+					newID = await (
+						await request(app).post("/api").send(sampleData)
+					).body._id;
+				}
+				response = await request(app).delete(`/api/:${newID}`);
+			});
+			test("should respond with 204 status code", () => {
+				expect(response.statusCode).toBe(204);
+			});
+			test("should remove matching record from database", async () => {
+				let newRecord = await request(app).get(`/api/search?_id=${newID}`);
+				expect(newRecord.statusCode).toBe(400);
+				expect(Object.keys(newRecord.body)[0]).toBe("error");
+			});
+		});
+		describe("when the id does not exist in the database", () => {
+			let response;
+			beforeAll(async () => {
+				response = await request(app).delete(`/api/:${badID}`);
+			});
+			test("should respond with 404 status code", () => {
+				expect(response.statusCode).toBe(404);
+			});
+			test("should respond with a JSON object", () => {
+				expect(response.headers["content-type"]).toEqual(
+					expect.stringContaining("json")
+				);
+			});
+			test("response object should contain an error message", () => {
+				expect(Object.keys(response.body)[0]).toEqual("error");
+			});
+		});
+	});
 });
